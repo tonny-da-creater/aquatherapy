1f793750f404e72ff2936ca31029fa2b58062fbe
tonny-da-creater <toni.belovanov96@mail.ru>
2022-07-21 16:57:29 +0300

    реализовал выезжающее меню из под шапки сайта, добавил прокрутку для меню для мобильной версии, добавил отмену прокрутки для контента при активном меню, уменьшил отступы блокам для мобильной версии

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,4 @@
 // настройки для слайдера - начало
-
 $(".carousel").slick({
   dots: true,
   centerMode: true,
@@ -15,17 +14,30 @@ $(".carousel").slick({
       settings: {
         slidesToShow: 1,
         variableWidth: false,
-
       }
     }
   ]
 });
-
 // настройки для слайдера - конец
 
 // состояние меню при отключённом js - начало
-
 let navMainContainer = document.querySelector('.main-nav__container');
 navMainContainer.classList.remove('main-nav__container--nojs');
-
 // состояние меню при отключённом js - конец
+
+// скрипт для переключения бургер меню и блокирования контента при открытом состоянии - начало
+let navToggle = document.querySelector('.main-nav__burger-menu');
+let body = document.querySelector('body');
+
+navToggle.addEventListener('click', function () {
+  if (navMainContainer.classList.contains('main-nav__container--closed')) {
+    navMainContainer.classList.remove('main-nav__container--closed');
+    navMainContainer.classList.add('main-nav__container--opened');
+    body.classList.add('lock');
+  } else {
+    navMainContainer.classList.add('main-nav__container--closed');
+    navMainContainer.classList.remove('main-nav__container--opened');
+    body.classList.remove('lock');
+  }
+});
+// скрипт для переключения бургер меню и блокирования контента при открытом состоянии - конец
